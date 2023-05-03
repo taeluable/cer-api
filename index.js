f5aece33a8b05dd1148849f51c8fde47845ddd0f
taeluable <132254857+taeluable@users.noreply.github.com>
2023-05-03 17:19:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const express = require('express')
-const app = express()
-const mysql = require('mysql2')
-const cors = require('cors')
-var bodyParser = require('body-parser')
-varjsonParser = bodyParser.json()
-require('dotenv').config()
-
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+require('dotenv').config();
 
 app.use(cors());
 
-const connection = mysql.createConnection(process.env.DATABASE_URL)
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
-app.get('/',(req,res) => {
-    console.log('hello')
-    res.send('hello')
-})
+app.get('/', (req, res) => {
+  console.log('hello');
+  res.send('hello');
+});
 
 app.get('/:x', (req, res) => {
   const x = req.params.x;
@@ -29,15 +28,11 @@ app.get('/:x', (req, res) => {
 
 app.post('/:x', jsonParser, function (req, res, next) {
   const x = req.params.x;
-  var Email = req.body.Email
-  res.json({ Email })
+  var Email = req.body.Email;
+  res.json({ Email });
   // process the POST request and update the database table
 });
 
-
-
-
- app.listen('3001', () => {
-    console.log('Sever is runnong on port 3001')
- })
-
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});

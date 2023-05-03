e085e404ab89be359d00346bb9b0a6cd8a647876
taeluable <132254857+taeluable@users.noreply.github.com>
2023-05-03 13:13:40 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql2')
+const cors = require('cors')
+require('dotenv').config()
+
+
+app.use(cors());
+
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+
+app.get('/',(req,res) => {
+    connection.query("SELECT * FROM audit",(err,result) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        } 
+    });
+});
+
+ app.listen('3001', () => {
+    console.log('Sever is runnong on port 3001')
+ })

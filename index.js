8afed5fc3dc724710224da001f22bab56aba0f2a
taeluable <132254857+taeluable@users.noreply.github.com>
2023-05-03 13:49:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,34 @@ app.use(cors());
 
 const connection = mysql.createConnection(process.env.DATABASE_URL)
 
-
-app.get('/audit',(req,res) => {
-    connection.query("SELECT * FROM audit",(err,result) => {
-        if(err){
-            console.log(err);
-        }else{
-            console.log('Connected to PlanetScale!')
-            res.send(result);
-        } 
-    })
-})
-
 app.get('/',(req,res) => {
     console.log('hello')
     res.send('hello')
-    })
+})
+
+app.get('/ocb', (req, res) => {
+  connection.query("SELECT table_name FROM information_schema.tables WHERE table_schema = '<ocb>'", (err, tables) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Connected to PlanetScale!');
+
+      let results = {};
+
+      tables.forEach(table => {
+        connection.query(`SELECT * FROM ${table.table_name}`, (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            results[table.table_name] = result;
+          }
+        });
+      });
+
+      res.send(results);
+    }
+ 
+
 
  app.listen('3001', () => {
     console.log('Sever is runnong on port 3001')

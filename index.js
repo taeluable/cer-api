942e4f79bf9acb0fea732e559f1f537f1f5ced83
taeluable <132254857+taeluable@users.noreply.github.com>
2023-05-03 17:05:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,34 +27,6 @@ app.get('/:x', (req, res) => {
   });
 });
 
-const express = require('express')
-const app = express()
-const mysql = require('mysql2')
-const cors = require('cors')
-var bodyParser = require('body-parser')
-varjsonParser = bodyParser.json()
-require('dotenv').config()
-
-
-app.use(cors());
-
-const connection = mysql.createConnection(process.env.DATABASE_URL)
-
-app.get('/',(req,res) => {
-    console.log('hello')
-    res.send('hello')
-})
-
-app.get('/:x', (req, res) => {
-  const x = req.params.x;
-  connection.query(`SELECT * FROM ${x}`, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    } 
-  });
-});
 
 app.post('/:x', jsonParser, function (req, res,next) => {
     const x = req.params.x;
@@ -78,17 +50,3 @@ app.put('/:x', (req, res) => {
     console.log('Sever is runnong on port 3001')
  })
 
-app.delete('/:x', (req, res) => {
-  const x = req.params.x;
-  // process the DELETE request and delete the corresponding record from the database table
-});
-
-app.put('/:x', (req, res) => {
-  const x = req.params.x;
-  // process the UPDATE request and update the corresponding record in the database table
-});
-
-
- app.listen('3001', () => {
-    console.log('Sever is runnong on port 3001')
- })
